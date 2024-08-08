ecde883740937cb2f23fc9e40a9cb7ef9ce213af
Aldyfaisal80 <98567736+Aldyfaisal80@users.noreply.github.com>
2024-08-08 11:14:36 +0700

    Update components and ignore pnpm-lock.yaml
    - Modified S1Navbar component
    - Modified MainLayout component
    - Modified Section1 component in sub-layout
    - Updated .gitignore to exclude pnpm-lock.yaml from version control

--- src/components/layouts/sub-layout/Section1.jsx
+++ src/components/layouts/sub-layout/Section1.jsx
@@ -1,5 +1,19 @@
 export default function Section1() {
   return (
-    <section> Nav+Hero </section>
+    <section className="flex">
+      <div className="mt-32 flex flex-col gap-10">
+        <h1 className="text-5xl font-bold leading-snug">Discover <span className="text-primary-blue">Furniture</span> <br /> With Ease Of Comfort.</h1>
+        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing <br /> elit, sed diam nonummy nibh euismod
+          tincidunt ut
+        </p>
+        <div className="flex items-center gap-4">
+          <div className="bg-primary-blue text-white p-2 rounded-lg w-[100px] flex items-center justify-center">Get Started</div>
+          <div className="text-primary-blue border-2 border-primary-blue p-[6px] w-[100px] flex items-center justify-center rounded-lg">Login</div>
+        </div>
+      </div>
+      <div className="right">
+        <img src="" alt="IMG" />
+      </div>
+    </section>
   )
 }
