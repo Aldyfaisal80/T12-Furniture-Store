192fa7c634c6af181de875111e4a8efc7bce2661
Aldyfaisal80 <98567736+Aldyfaisal80@users.noreply.github.com>
2024-08-08 22:10:28 +0700

    footer

--- src/components/fragments/S4Footer.jsx
+++ src/components/fragments/S4Footer.jsx
@@ -1,5 +1,15 @@
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div className="flex">
+      <div className="flex flex-col gap-6">
+        <h1 className="text-3xl font-bold italic">FURNITURE<span className="text-gray-500">.ME</span></h1>
+        <p className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. <br /> At itaque sed aperiam dolorum soluta debitis mollitia <br /> eum perspiciatis fugit, suscipit cum learn more <span className="underline">Learn More</span></p>
+        <h1 className="text-2xl font-semibold">Morning Hours</h1>
+        <span>Saturday - Thusday <span>Friday</span></span>
+      </div>
+      <div className="flex flex-col">
+        
+      </div>
+    </div>
   )
 }
