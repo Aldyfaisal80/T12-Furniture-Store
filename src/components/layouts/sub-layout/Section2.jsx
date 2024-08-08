1ab4121eb0253f7abbec4f00386d7134f9d7c09c
rafi andrea <rafiandrealesmana@gmail.com>
2024-08-08 16:07:42 +0700

    add img

--- src/components/layouts/sub-layout/Section2.jsx
+++ src/components/layouts/sub-layout/Section2.jsx
@@ -1,5 +1,10 @@
 export default function Section2() {
     return (
-        <section> card popular product </section>
+        <section>
+            <div>
+                <h1>Popular Products</h1>
+                <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, voluptatem nesciunt <br/> minima natus deserunt odit.</span>
+            </div>
+        </section>
     )
 }
