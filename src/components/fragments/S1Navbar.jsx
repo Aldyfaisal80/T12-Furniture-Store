0c03f18566e08b45d2649c2fb6dc11cf657804a6
Aldyfaisal80 <98567736+Aldyfaisal80@users.noreply.github.com>
2024-08-10 21:04:31 +0700

    make hover

--- src/components/fragments/S1Navbar.jsx
+++ src/components/fragments/S1Navbar.jsx
@@ -20,19 +20,19 @@ export default function Navbar() {
           <option value="">Table</option>
         </select>
         <ul className="flex gap-6">
-          <li className="relative text-black cursor-pointer transition-all ease-in-out hover:text-blue-500 before:transition-[width] before:ease-in-out before:duration-700 before:absolute before:bg-blue-500 before:origin-center before:h-[2px] before:w-0 hover:before:w-[50%] before:bottom-0 before:left-[50%] after:transition-[width] after:ease-in-out after:duration-700 after:absolute after:bg-blue-500 after:origin-center after:h-[2px] after:w-0 hover:after:w-[50%] after:bottom-0 after:right-[50%]">
+          <li className="relative text-black cursor-pointer transition-all ease-in-out hover-underline">
             <a href="#">Home</a>
           </li>
-          <li className="relative text-black cursor-pointer transition-all ease-in-out hover:text-blue-500 before:transition-[width] before:ease-in-out before:duration-700 before:absolute before:bg-blue-500 before:origin-center before:h-[2px] before:w-0 hover:before:w-[50%] before:bottom-0 before:left-[50%] after:transition-[width] after:ease-in-out after:duration-700 after:absolute after:bg-blue-500 after:origin-center after:h-[2px] after:w-0 hover:after:w-[50%] after:bottom-0 after:right-[50%]">
+          <li className="relative text-black cursor-pointer transition-all ease-in-out hover-underline">
             <a href="#">Services</a>
           </li>
-          <li className="relative text-black cursor-pointer transition-all ease-in-out hover:text-blue-500 before:transition-[width] before:ease-in-out before:duration-700 before:absolute before:bg-blue-500 before:origin-center before:h-[2px] before:w-0 hover:before:w-[50%] before:bottom-0 before:left-[50%] after:transition-[width] after:ease-in-out after:duration-700 after:absolute after:bg-blue-500 after:origin-center after:h-[2px] after:w-0 hover:after:w-[50%] after:bottom-0 after:right-[50%]">
+          <li className="relative text-black cursor-pointer transition-all ease-in-out hover-underline">
             <a href="#">Blog</a>
           </li>
-          <li className="relative text-black cursor-pointer transition-all ease-in-out hover:text-blue-500 before:transition-[width] before:ease-in-out before:duration-700 before:absolute before:bg-blue-500 before:origin-center before:h-[2px] before:w-0 hover:before:w-[50%] before:bottom-0 before:left-[50%] after:transition-[width] after:ease-in-out after:duration-700 after:absolute after:bg-blue-500 after:origin-center after:h-[2px] after:w-0 hover:after:w-[50%] after:bottom-0 after:right-[50%]">
+          <li className="relative text-black cursor-pointer transition-all ease-in-out hover-underline">
             <a href="#">Contact</a>
           </li>
-          <li className="relative text-black cursor-pointer transition-all ease-in-out hover:text-blue-500 before:transition-[width] before:ease-in-out before:duration-700 before:absolute before:bg-blue-500 before:origin-center before:h-[2px] before:w-0 hover:before:w-[50%] before:bottom-0 before:left-[50%] after:transition-[width] after:ease-in-out after:duration-700 after:absolute after:bg-blue-500 after:origin-center after:h-[2px] after:w-0 hover:after:w-[50%] after:bottom-0 after:right-[50%]">
+          <li className="relative text-black cursor-pointer transition-all ease-in-out hover-underline">
             <a href="#">About us</a>
           </li>
         </ul>
